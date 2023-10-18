baf0516dcf6cd995f9cbb5aef5fddc168babb525
djdongjae <jk47wee@gmail.com>
2023-10-18 10:13:42 +0900

    two

--- src/f201914089/Main.java
+++ src/f201914089/Main.java
@@ -2,7 +2,8 @@ package f201914089;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("one 201914089");
-    }
+  public static void main(String[] args) {
+    Two.print();
+    System.out.println("one 201914089");
+  }
 }
